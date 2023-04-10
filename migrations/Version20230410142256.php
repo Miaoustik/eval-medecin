4bea2bdd35a8df383dddbcb8e411765553f446c0
Miaoustik <cedricwat@outlook.fr>
2023-04-10 16:23:42 +0200

    fix migration

---
+++ migrations/Version20230410142256.php
@@ -0,0 +1,124 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230410142256 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE allergen_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE contact_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE diet_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE ingredient_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE ingredient_recipe_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE notice_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE recipe_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE user_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE allergen (id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_25BF08CE5E237E06 ON allergen (name)');
+        $this->addSql('CREATE TABLE contact (id INT NOT NULL, message TEXT NOT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE diet (id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_9DE465205E237E06 ON diet (name)');
+        $this->addSql('CREATE TABLE ingredient (id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_6BAF78705E237E06 ON ingredient (name)');
+        $this->addSql('CREATE TABLE ingredient_recipe (id INT NOT NULL, ingredient_id INT NOT NULL, recipe_id INT NOT NULL, quantity VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_36F27176933FE08C ON ingredient_recipe (ingredient_id)');
+        $this->addSql('CREATE INDEX IDX_36F2717659D8A214 ON ingredient_recipe (recipe_id)');
+        $this->addSql('CREATE TABLE notice (id INT NOT NULL, recipe_id INT NOT NULL, user_id INT NOT NULL, note INT NOT NULL, content TEXT DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_480D45C259D8A214 ON notice (recipe_id)');
+        $this->addSql('CREATE INDEX IDX_480D45C2A76ED395 ON notice (user_id)');
+        $this->addSql('COMMENT ON COLUMN notice.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE recipe (id INT NOT NULL, title VARCHAR(255) NOT NULL, description TEXT NOT NULL, preparation_time INT NOT NULL, break_time INT NOT NULL, cooking_time INT NOT NULL, patient_only BOOLEAN NOT NULL, stages JSON NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE recipe_diet (recipe_id INT NOT NULL, diet_id INT NOT NULL, PRIMARY KEY(recipe_id, diet_id))');
+        $this->addSql('CREATE INDEX IDX_E2FF3FFF59D8A214 ON recipe_diet (recipe_id)');
+        $this->addSql('CREATE INDEX IDX_E2FF3FFFE1E13ACE ON recipe_diet (diet_id)');
+        $this->addSql('CREATE TABLE recipe_allergen (recipe_id INT NOT NULL, allergen_id INT NOT NULL, PRIMARY KEY(recipe_id, allergen_id))');
+        $this->addSql('CREATE INDEX IDX_532FAD5059D8A214 ON recipe_allergen (recipe_id)');
+        $this->addSql('CREATE INDEX IDX_532FAD506E775A4A ON recipe_allergen (allergen_id)');
+        $this->addSql('CREATE TABLE "user" (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON "user" (email)');
+        $this->addSql('CREATE TABLE user_allergen (user_id INT NOT NULL, allergen_id INT NOT NULL, PRIMARY KEY(user_id, allergen_id))');
+        $this->addSql('CREATE INDEX IDX_C532ECCEA76ED395 ON user_allergen (user_id)');
+        $this->addSql('CREATE INDEX IDX_C532ECCE6E775A4A ON user_allergen (allergen_id)');
+        $this->addSql('CREATE TABLE user_diet (user_id INT NOT NULL, diet_id INT NOT NULL, PRIMARY KEY(user_id, diet_id))');
+        $this->addSql('CREATE INDEX IDX_E76529E9A76ED395 ON user_diet (user_id)');
+        $this->addSql('CREATE INDEX IDX_E76529E9E1E13ACE ON user_diet (diet_id)');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
+        $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
+        $this->addSql('CREATE INDEX IDX_75EA56E016BA31DB ON messenger_messages (delivered_at)');
+        $this->addSql('CREATE OR REPLACE FUNCTION notify_messenger_messages() RETURNS TRIGGER AS $$
+            BEGIN
+                PERFORM pg_notify(\'messenger_messages\', NEW.queue_name::text);
+                RETURN NEW;
+            END;
+        $$ LANGUAGE plpgsql;');
+        $this->addSql('DROP TRIGGER IF EXISTS notify_trigger ON messenger_messages;');
+        $this->addSql('CREATE TRIGGER notify_trigger AFTER INSERT OR UPDATE ON messenger_messages FOR EACH ROW EXECUTE PROCEDURE notify_messenger_messages();');
+        $this->addSql('ALTER TABLE ingredient_recipe ADD CONSTRAINT FK_36F27176933FE08C FOREIGN KEY (ingredient_id) REFERENCES ingredient (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE ingredient_recipe ADD CONSTRAINT FK_36F2717659D8A214 FOREIGN KEY (recipe_id) REFERENCES recipe (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE notice ADD CONSTRAINT FK_480D45C259D8A214 FOREIGN KEY (recipe_id) REFERENCES recipe (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE notice ADD CONSTRAINT FK_480D45C2A76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE recipe_diet ADD CONSTRAINT FK_E2FF3FFF59D8A214 FOREIGN KEY (recipe_id) REFERENCES recipe (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE recipe_diet ADD CONSTRAINT FK_E2FF3FFFE1E13ACE FOREIGN KEY (diet_id) REFERENCES diet (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE recipe_allergen ADD CONSTRAINT FK_532FAD5059D8A214 FOREIGN KEY (recipe_id) REFERENCES recipe (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE recipe_allergen ADD CONSTRAINT FK_532FAD506E775A4A FOREIGN KEY (allergen_id) REFERENCES allergen (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE user_allergen ADD CONSTRAINT FK_C532ECCEA76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE user_allergen ADD CONSTRAINT FK_C532ECCE6E775A4A FOREIGN KEY (allergen_id) REFERENCES allergen (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE user_diet ADD CONSTRAINT FK_E76529E9A76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE user_diet ADD CONSTRAINT FK_E76529E9E1E13ACE FOREIGN KEY (diet_id) REFERENCES diet (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE allergen_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE contact_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE diet_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE ingredient_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE ingredient_recipe_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE notice_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE recipe_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE user_id_seq CASCADE');
+        $this->addSql('ALTER TABLE ingredient_recipe DROP CONSTRAINT FK_36F27176933FE08C');
+        $this->addSql('ALTER TABLE ingredient_recipe DROP CONSTRAINT FK_36F2717659D8A214');
+        $this->addSql('ALTER TABLE notice DROP CONSTRAINT FK_480D45C259D8A214');
+        $this->addSql('ALTER TABLE notice DROP CONSTRAINT FK_480D45C2A76ED395');
+        $this->addSql('ALTER TABLE recipe_diet DROP CONSTRAINT FK_E2FF3FFF59D8A214');
+        $this->addSql('ALTER TABLE recipe_diet DROP CONSTRAINT FK_E2FF3FFFE1E13ACE');
+        $this->addSql('ALTER TABLE recipe_allergen DROP CONSTRAINT FK_532FAD5059D8A214');
+        $this->addSql('ALTER TABLE recipe_allergen DROP CONSTRAINT FK_532FAD506E775A4A');
+        $this->addSql('ALTER TABLE user_allergen DROP CONSTRAINT FK_C532ECCEA76ED395');
+        $this->addSql('ALTER TABLE user_allergen DROP CONSTRAINT FK_C532ECCE6E775A4A');
+        $this->addSql('ALTER TABLE user_diet DROP CONSTRAINT FK_E76529E9A76ED395');
+        $this->addSql('ALTER TABLE user_diet DROP CONSTRAINT FK_E76529E9E1E13ACE');
+        $this->addSql('DROP TABLE allergen');
+        $this->addSql('DROP TABLE contact');
+        $this->addSql('DROP TABLE diet');
+        $this->addSql('DROP TABLE ingredient');
+        $this->addSql('DROP TABLE ingredient_recipe');
+        $this->addSql('DROP TABLE notice');
+        $this->addSql('DROP TABLE recipe');
+        $this->addSql('DROP TABLE recipe_diet');
+        $this->addSql('DROP TABLE recipe_allergen');
+        $this->addSql('DROP TABLE "user"');
+        $this->addSql('DROP TABLE user_allergen');
+        $this->addSql('DROP TABLE user_diet');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
